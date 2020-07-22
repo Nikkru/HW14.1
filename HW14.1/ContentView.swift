2844745e3003fe602442605f82484f8a8ed791be
Nikkru <56563372+Nikkru@users.noreply.github.com>
2020-07-22 17:10:42 +0300

    Initial Commit

---
+++ HW14.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW14.1
+//
+//  Created by Nikolai Krusser on 22.07.2020.
+//  Copyright © 2020 Nikolai Krusser. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
